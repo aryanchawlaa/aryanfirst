80233c63109aee9092b5c51206c7762c8c93dd4d
aryanchawlaa <aryancr7rko@gmail.com>
2024-06-17 23:09:46 +0530

    bhag ayah se

---
+++ hello/hello.cpp
@@ -0,0 +1,18 @@
+include <iostream>
+using namespace std; 
+int main()
+{
+int age;
+  cout<<"enter your age ";
+  cin>>age;
+  if(age>18){
+    cout<<"minor";
+}
+  else if(age<18){
+    cout<<"legal";
+    }
+return 0 ;
+  
+
+  
+}
